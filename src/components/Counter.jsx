e50525360ac37f90b92fcfe589e84875e5188f64
Tom Hermans <tomhermans@gmail.com>
2021-07-18 20:53:48 +0200

    added a simple counter btn component

---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div style={{ margin: "20px 0" }}>
+      <h1>Simple React Counter</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => {
+        setCount(count + 1)
+      }
+      }>+</button>
+      <button onClick={() => {
+        setCount(count - 1)
+      }
+      }>-</button>
+    </div>
+  )
+}
